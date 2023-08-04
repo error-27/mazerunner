ec8ac75c59940448ee81bc114083a3c8362dfd47
[Error_27] <error27dev@gmail.com>
2023-08-04 18:20:10 -0400

    Initial commit

---
+++ src/rat.rs
@@ -0,0 +1,27 @@
+// Functions and datatypes for the rat
+
+struct Rat {
+    x: u8,
+    y: u8,
+
+}
+
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+}
+
+impl Direction {
+    fn left(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Left => Direction::Down,
+            Direction::Down => Direction::Right,
+            Direction::Right => Direction::Up,
+
+            _ => eprintln!("how the hell did you make a fifth cardinal direction") // Literally impossible to get this
+        }
+    }
+}
