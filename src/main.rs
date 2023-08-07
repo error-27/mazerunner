6aaaf87bfb6fcf4ddc7070746c32e1bc74d80908
[Error_27] <error27dev@gmail.com>
2023-08-07 18:04:09 -0400

    clean up unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 mod parse;
 
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, Result, BufReader, Lines};
-use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
